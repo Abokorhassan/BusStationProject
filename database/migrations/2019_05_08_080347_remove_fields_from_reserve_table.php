ec4d6293a5dc40c2159d2e91ca8159b5c520823a
abokorhassan <abokor13@hotmail.com>
2019-05-08 11:27:56 +0300

    removing felds didnt work in migaration

---
+++ database/migrations/2019_05_08_080347_remove_fields_from_reserve_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveFieldsFromReserveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reserves', function($table) {
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('third_name');
+            $table->dropColumn('ph_number');
+         });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reserves', function($table) {
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('third_name');
+            $table->integer('ph_number');
+         });   
+    }
+}
